b674b718ba8bd5b16fb66ba02b656c929a38cf2e
infinity <abhishek8developer@gmail.com>
2018-03-23 08:24:34 +0530

    added 3 section on home page

--- src/modules/Home/components/LandingBanner.js
+++ src/modules/Home/components/LandingBanner.js
@@ -1,10 +1,26 @@
 import React from 'react';
 
 const LandingBanner = () => (
-  <div className='col-xs-12 landing-banner'>
+  <div className='col-xs-12'>
     <div className='row'>
-      <div className='col-xs-12'>
+      <div className='col-xs-12 landing-banner'>
         asdsadasdas
+      </div>
+    </div>
+    <div className='row'>
+      <div className='col-xs-12 about-us'>
+        <h2>Manage Tasks, Create Deadlines to Meet.</h2>
+        <p>Task Scheduler has everything you need to get your new task up & running in no time!</p>
+        <p>All of the templates and themes on Start Bootstrap are open source, free to download,</p>
+        <p>and easy to use. No strings attached!</p>
+      </div>
+    </div>
+    <div className='row'>
+      <div className='col-xs-12 logo-branding'>
+        <h2>Manage Tasks, Create Deadlines to Meet.</h2>
+        <p>Task Scheduler has everything you need to get your new task up & running in no time!</p>
+        <p>All of the templates and themes on Start Bootstrap are open source, free to download,</p>
+        <p>and easy to use. No strings attached!</p>
       </div>
     </div>
   </div>
