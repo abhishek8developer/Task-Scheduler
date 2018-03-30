ab936d22bd733b53af1a57c20aa3f86c5ec92ee4
abhishek8developer <abhishek8developer@gmail.com>
2018-03-30 16:30:48 +0530

    added google login

--- src/modules/Login/containers/index.js
+++ src/modules/Login/containers/index.js
@@ -2,7 +2,8 @@ import React, { PropTypes } from 'react';
 import { connect } from 'react-redux';
 import LoginForm from '../components/';
 import { login } from 'Redux/login/actions/';
-
+import { GoogleLogin } from 'react-google-login';
+import FacebookLogin from 'react-facebook-login';
 
 export class Login extends React.Component {
   constructor(props) {
@@ -12,9 +13,16 @@ export class Login extends React.Component {
       pass: '123'
     };
     this.onSubmit = this.onSubmit.bind(this);
+    this.responseGoogle = this.responseGoogle.bind(this);;
   }
   onSubmit() {
     this.props.onSubmit(this.state);
+  }
+  facebookLogin() {
+    // appId: '214316022652701'
+  }
+  responseGoogle(response) {
+    console.log(response);
   }
   render() {
     console.log(this.props.name);
@@ -26,6 +34,18 @@ export class Login extends React.Component {
           onNameChange={(e) => this.setState({ name: e.target.value })}
           onPassChange={(e) => this.setState({ pass: e.target.value })}
           onSubmit={() => this.onSubmit}
+        />
+        <GoogleLogin
+          clientId='227375302353-gbh8gt56s6chqhvdp6j1sotoc78eh0jg.apps.googleusercontent.com'
+          buttonText='Login'
+          onSuccess={this.responseGoogle}
+          onFailure={this.responseGoogle}
+        />
+        <FacebookLogin
+          appId='214316022652701'
+          autoLoad
+          fields='name,email,picture'
+          callback={this.responseGoogle}
         />
         <div>
           {data ? data.map((val, i) => (
